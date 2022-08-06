0d14821f19fcb6ed04b657b677bd998ad1113fcb
David <davidrlutrick@gmail.com>
2022-08-06 11:53:52 -0700

    changed App.css to .scss and updated in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, React</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
